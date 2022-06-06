f7cccc3c4982affe32785484dd520f4012f6b38f
AYUSHBANSAL-hub <72022069+AYUSHBANSAL-hub@users.noreply.github.com>
2022-06-06 11:25:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,5 @@ app.use(express.static(__dirname))
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/main.html'));
 });
-//your code here
-app.post('/add', (req, res) => {
-  const {a,b} = req.body;
-  res.status(200).send(a+b);
-  // res.sendFile(path.join(__dirname + '/main.html'));
-});
+
 module.exports = app;
